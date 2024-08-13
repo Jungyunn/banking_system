48b3861b3b5d37f0ecf2afe5142105ea16f9ea67
Kwonohs <dhtpa95@gmail.com>
2024-08-13 17:56:35 +0900

    committttasdasd

--- deposit.py
+++ deposit.py
@@ -6,7 +6,7 @@ def deposit_money():
 
     for i in range(len(account_list)):
         if account_id == account_list[i]:
-            account_list[i].balance -= deposit_money
+            account_list[i].balance += deposit_money
             print("입금완료")
             return
     print("유효하지 않는 ID입니다.")
--- withdraw.py
+++ withdraw.py
@@ -6,7 +6,7 @@ def withdraw_money():
     withdraw_money_ = int(input("출금액: "))
 
     for i in range(len(account_list)):
-        if account_id == account_list[i]:
+        if account_id == account_list[i].account_id:
             if account_list[i].balance >= withdraw_money_:
                 account_list[i].balance -= withdraw_money_
                 print("출금완료")
