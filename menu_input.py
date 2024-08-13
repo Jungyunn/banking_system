b1f6f5a381462f05aad90839ce62e5475cb42cde
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-08-13 16:42:31 +0900

    feat: menu 관련 출력 함수 추가

--- menu_input.py
+++ menu_input.py
@@ -1,3 +1,12 @@
+def show_menu():
+  print("-----Menu------")
+  print("1. 계좌개설")
+  print("2. 입    금")
+  print("3. 출    금")
+  print("4. 계좌정보 전체 출력")
+  print("5. 프로그램 종료 ")
+
+
 def menu_input():
     user_num = int(input("메뉴에서 필요하신 항목을 선택해주세요"))
     if user_num < 0 or user_num >= 6:
