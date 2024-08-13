997e5bb91efda1d4248a0887426f605ad61f6cca
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-08-13 17:41:53 +0900

    feat: can use account_list yaho

--- account_list.py
+++ account_list.py
@@ -1,5 +1,2 @@
-class AccountList:
-    def __init__(self):
-        self.account_list = []
-        self.num_of_accounts = 0
-    
+account_list = []
+num_of_accounts = 0
--- deposit.py
+++ deposit.py
@@ -1,12 +1,12 @@
-from account_list import AccountList
+from account_list import account_list
 
 def deposit_money():
-    account_id = int(input("계좌ID: "))
+    account_id = input("계좌ID: ")
     deposit_money = int(input("입금액: "))
 
-    for i in range(len(AccountList.account_list)):
-        if account_id == AccountList.account_list[i]:
-            AccountList.account_list[i].balance -= deposit_money
+    for i in range(len(account_list)):
+        if account_id == account_list[i]:
+            account_list[i].balance -= deposit_money
             print("입금완료")
             return
     print("유효하지 않는 ID입니다.")
